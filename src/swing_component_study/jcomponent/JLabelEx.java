4b3765be447427fb10506f63ba3ac28d9ebd5a67
rlawpdud301 <rlawpdud301@naver.com>
2018-11-06 16:44:32 +0900

    이미지아이콘 실행 경로변경!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!및레이블추가

---
+++ src/swing_component_study/jcomponent/JLabelEx.java
@@ -0,0 +1,61 @@
+package swing_component_study.jcomponent;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+public class JLabelEx extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JLabelEx frame = new JLabelEx();
+					frame.setVisible(true);
+					
+					String dir = System.getProperty("user.dir")+"\\images";
+					System.out.println(dir);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JLabelEx() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 443, 589);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel txtLable = new JLabel("사랑합니다");
+		contentPane.add(txtLable);
+		
+		JLabel lblNewLabel = new JLabel("");
+		String iconPath1 = System.getProperty("user.dir")+"\\images\\";
+		lblNewLabel.setIcon(new ImageIcon(iconPath1+"beauty.jpg"));
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("보고싶으면 카톡하세요");
+		lblNewLabel_1.setIcon(new ImageIcon(iconPath1 + "normalIcon.gif"));
+		contentPane.add(lblNewLabel_1);
+	}
+
+}
